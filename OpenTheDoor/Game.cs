f5a10d15de3a6b1340c2bb058ba38f89be67d217
unknown <mszauer@mail.usf.edu>
2015-09-13 12:27:33 -0500

    initial commit for OpenTheDoor

---
+++ OpenTheDoor/Game.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using GameFramework;
+using System.Drawing;
+using OpenTK;
+
+namespace OpenTheDoor {
+    class Game {
+        protected Point spawnTile = new Point(2, 1);
+        protected PlayerCharacter hero = null;
+        protected string heroSheet = "Assets/Link.png";
+        public OpenTK.GameWindow Window = null;
+        protected Tile[][] map = null;
+        protected int[][] mapLayout = new int[][] {
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 1, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 1, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+
+        protected string spriteSheets = "Assets/HouseTiles.png";
+        protected Rectangle[] spriteSources = new Rectangle[] {
+            new Rectangle(466,32,30,30),
+            new Rectangle(466,1,30,30)
+        };
+        Tile[][] GenerateMap(int[][] layout, string sheets, Rectangle[] sources) {
+            Tile[][] result = new Tile[layout.Length][];
+            float scale = 1.0f;
+            for (int i = 0; i < layout.Length; i++) {
+                result[i] = new Tile[layout[i].Length];
+
+                for (int j = 0; j < layout[i].Length; j++) {
+                    Rectangle source = sources[layout[i][j]];
+
+                    Point worldPosition = new Point();
+                    worldPosition.X = (int)(j * source.Width);
+                    worldPosition.Y = (int)(i * source.Height);
+                    result[i][j] = new Tile(sheets, source);
+                    result[i][j].Walkable = layout[i][j] == 0;
+                    result[i][j].WorldPosition = worldPosition;
+                    result[i][j].Scale = scale;
+                }
+            }
+            return result;
+        }
+        public Tile GetTile(PointF pixelPoint) {
+            return map[(int)pixelPoint.Y / 30][(int)pixelPoint.X / 30];
+        }
+        public Rectangle GetTileRect(PointF pixelPoint) {
+            int xTile = (int)pixelPoint.X / 30;//integer math
+            int yTile = (int)pixelPoint.Y / 30;
+            Rectangle result = new Rectangle(xTile * 30, yTile * 30, 30, 30);
+            return result;
+        }
+        //Singleton
+        private static Game instance = null;
+        public static Game Instance {
+            get {
+                if (instance == null) {
+                    instance = new Game();
+                }
+                return instance;
+            }
+        }
+
+        protected Game() {
+
+        }
+        public void Initialize(OpenTK.GameWindow window) {
+            Window = window;
+            window.ClientSize = new Size(mapLayout[0].Length * 30, mapLayout.Length * 30);
+            TextureManager.Instance.UseNearestFiltering = true;
+            map = GenerateMap(mapLayout, spriteSheets, spriteSources);
+            hero = new PlayerCharacter(heroSheet, new Point(spawnTile.X * 30, spawnTile.Y * 30));
+
+
+        }
+        public void Update(float dt) {
+
+            hero.Update(dt);
+        }
+        public void Render() {
+            for (int h = 0; h < map.Length; h++) {
+                for (int w = 0; w < map[h].Length; w++) {
+                    map[h][w].Render();
+                }
+            }
+            /*
+            foreach (PointF corner in hero.Corners) {
+                if (!GetTile(corner).Walkable) {
+                    GraphicsManager.Instance.DrawRect(GetTileRect(corner), Color.Violet);
+                }
+                else {
+                    GraphicsManager.Instance.DrawRect(GetTileRect(corner), Color.Blue);
+                }
+            }*/
+            hero.Render();
+
+        }
+        public void Shutdown() {
+            for (int h = 0; h < map.Length; h++) {
+                for (int w = 0; w < map[h].Length; w++) {
+                    map[h][w].Destroy();
+                }
+            }
+            hero.Destroy();
+        }
+    }
+}
---
+++ OpenTheDoor/PlayerCharacter.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameFramework;
+using System.Drawing;
+
+namespace OpenTheDoor {
+    class PlayerCharacter :Character {
+        public float speed = 90.0f;
+        float animFPS = 1.0f / 9.0f;
+        float animTimer = 0f;
+        public PlayerCharacter(string spritePath,Point pos) : base(spritePath, pos) {
+            AddSprite("Down", new Rectangle(59, 1, 24, 30), new Rectangle(87, 1, 24, 30));
+            AddSprite("Up", new Rectangle(115, 3, 22, 28), new Rectangle(141, 3, 22, 30));
+            AddSprite("Left", new Rectangle(1, 1, 26, 30), new Rectangle(31, 1, 26, 30));
+            AddSprite("Right", new Rectangle(195, 1, 26, 30), new Rectangle(167, 1, 26, 30));
+            SetSprite("Down");
+        }
+        public void Update(float deltaTime) {
+            InputManager i = InputManager.Instance;
+
+            if (i.KeyDown(OpenTK.Input.Key.A) || i.KeyDown(OpenTK.Input.Key.Left)) {
+                SetSprite("Left");
+                Animate(deltaTime);
+                Position.X -= speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Right;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Right;
+                    }
+                }
+            }
+            else if (i.KeyDown(OpenTK.Input.Key.D) || i.KeyDown(OpenTK.Input.Key.Right)) {
+                SetSprite("Right");
+                Animate(deltaTime);
+                Position.X += speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Left - Rect.Width;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Left - Rect.Width;
+                    }
+                }
+            }
+            if (i.KeyDown(OpenTK.Input.Key.W) || i.KeyDown(OpenTK.Input.Key.Up)) {
+                SetSprite("Up");
+                Animate(deltaTime);
+                Position.Y -= speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
+            }
+            else if (i.KeyDown(OpenTK.Input.Key.S) || i.KeyDown(OpenTK.Input.Key.Down)) {
+                SetSprite("Down");
+                Animate(deltaTime);
+                Position.Y += speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
+            }
+
+        }
+        protected void Animate(float dTime) {
+            animTimer += dTime;
+            if (animTimer > animFPS) {
+                animTimer -= animFPS;
+                currentFrame += 1;
+                if (currentFrame > SpriteSources[currentSprite].Length-1) {
+                    currentFrame = 0;
+                }
+            }
+        }
+    }
+}
