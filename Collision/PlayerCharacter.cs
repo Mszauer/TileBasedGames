9dbac877ac373f77f7bbcab99b030b2aaed83762
unknown <mszauer@mail.usf.edu>
2015-09-12 22:45:11 -0500

    finished collisions

--- Collision/PlayerCharacter.cs
+++ Collision/PlayerCharacter.cs
@@ -14,9 +14,9 @@ namespace Collision {
         float animTimer = 0.0f;
         public PlayerCharacter(string spritePath, Point pos) : base(spritePath,pos) {
             AddSprite("Down", new Rectangle(59, 1, 24, 30), new Rectangle(87, 1, 24, 30));
-            AddSprite("Up", new Rectangle(115, 3, 22, 28), new Rectangle(141, 3, 22, 28));
-            AddSprite("Left", new Rectangle(1, 1, 26, 30), new Rectangle(31, 1, 26, 31));
-            AddSprite("Right", new Rectangle(195, 1, 26, 30), new Rectangle(167, 1, 26, 29));
+            AddSprite("Up", new Rectangle(115, 3, 22, 28), new Rectangle(141, 3, 22, 30));
+            AddSprite("Left", new Rectangle(1, 1, 26, 30), new Rectangle(31, 1, 26, 30));
+            AddSprite("Right", new Rectangle(195, 1, 26, 30), new Rectangle(167, 1, 26, 30));
             SetSprite("Down");
         }
         public void Update(float deltaTime) {
@@ -46,13 +46,13 @@ namespace Collision {
                 if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
                     Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
                     if (intersection.Width*intersection.Height>0){
-                        Position.X = intersection.Left-30;
+                        Position.X = intersection.Left-Rect.Width;
                     }
                 }
                 if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
                     Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
                     if (intersection.Width*intersection.Height > 0){
-                        Position.X = intersection.Left - 30;
+                        Position.X = intersection.Left - Rect.Width;
                     }
                 }
             }
@@ -60,11 +60,35 @@ namespace Collision {
                 SetSprite("Up");
                 Animate(deltaTime);
                 Position.Y -= speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                    Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
             }
             else if (i.KeyDown(OpenTK.Input.Key.S) || i.KeyDown(OpenTK.Input.Key.Down)) {
                 SetSprite("Down");
                 Animate(deltaTime);
                 Position.Y += speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                    Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersection.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
             }
 
         }
