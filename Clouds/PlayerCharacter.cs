7bc8ebfa67d8e5a7c082050bfeff5db47fc03767
unknown <mszauer@mail.usf.edu>
2015-09-17 17:13:33 -0500

    initial commit

---
+++ Clouds/PlayerCharacter.cs
@@ -0,0 +1,168 @@
+﻿#define ENABLE_VERTICAL_MOVEMENT
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameFramework;
+using System.Drawing;
+
+namespace Clouds {
+    class PlayerCharacter : Character {
+        public float speed = 90.0f;
+        float animFPS = 1.0f / 9.0f;
+        float animTimer = 0f;
+        protected float impulse = 0.0f;
+        protected float velocity = 0.0f;
+        protected float gravity = 0 / 0f;
+        public PlayerCharacter(string spritePath, Point pos) : base(spritePath, pos) {
+            AddSprite("Down", new Rectangle(59, 1, 24, 30), new Rectangle(87, 1, 24, 30));
+            AddSprite("Up", new Rectangle(115, 3, 22, 30), new Rectangle(141, 3, 22, 30));
+            AddSprite("Left", new Rectangle(1, 1, 26, 30), new Rectangle(31, 1, 26, 30));
+            AddSprite("Right", new Rectangle(195, 1, 26, 30), new Rectangle(167, 1, 26, 30));
+            AddSprite("Jump", new Rectangle(122, 75, 23, 30), new Rectangle(154, 76, 23, 30));
+            SetSprite("Down");
+            SetJump(3 * 30, 0.75f);
+        }
+        public void Update(float deltaTime) {
+            InputManager i = InputManager.Instance;
+
+            if (i.KeyDown(OpenTK.Input.Key.A) || i.KeyDown(OpenTK.Input.Key.Left)) {
+                if (velocity == gravity) {
+                    SetSprite("Left");
+                }
+                Animate(deltaTime);
+                Position.X -= speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Right;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Right;
+                    }
+                }
+            }
+            else if (i.KeyDown(OpenTK.Input.Key.D) || i.KeyDown(OpenTK.Input.Key.Right)) {
+                if (velocity == gravity) {
+                    SetSprite("Right");
+                }
+                Animate(deltaTime);
+                Position.X += speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Left - Rect.Width;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.X = intersection.Left - Rect.Width;
+                    }
+                }
+            }
+#if ENABLE_VERTICAL_MOVEMENT
+            if (i.KeyDown(OpenTK.Input.Key.W) || i.KeyDown(OpenTK.Input.Key.Up)) {
+                SetSprite("Up");
+                Animate(deltaTime);
+                Position.Y -= speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Bottom;
+                    }
+                }
+            }
+            else if (i.KeyDown(OpenTK.Input.Key.S) || i.KeyDown(OpenTK.Input.Key.Down)) {
+                SetSprite("Down");
+                Animate(deltaTime);
+                Position.Y += speed * deltaTime;
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
+                if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                    Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                    if (intersection.Width * intersection.Height > 0) {
+                        Position.Y = intersection.Top - Rect.Height;
+                    }
+                }
+            }
+#else
+            if (i.KeyPressed(OpenTK.Input.Key.Space) && velocity == gravity) {
+                velocity = impulse;
+                SetSprite("Jump");
+            }
+            if (velocity != gravity) {
+                Animate(deltaTime);
+            }
+            velocity += gravity * deltaTime;
+            if (velocity > gravity) {
+                velocity = gravity;
+            }
+            Position.Y += velocity * deltaTime;
+            if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_LEFT]).Walkable) {
+                Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_LEFT]));
+                if (intersection.Width * intersection.Height > 0) {
+                    Position.Y = intersection.Top - Rect.Height;
+                    if (velocity != gravity) {
+                        SetSprite("Down");
+                    }
+                    velocity = gravity;
+                }
+            }
+            if (!Game.Instance.GetTile(Corners[CORNER_BOTTOM_RIGHT]).Walkable) {
+                Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_BOTTOM_RIGHT]));
+                if (intersection.Width * intersection.Height > 0) {
+                    Position.Y = intersection.Top - Rect.Height;
+                    if (velocity != gravity) {
+                        SetSprite("Down");
+                    }
+                    velocity = gravity;
+                }
+            }
+            if (!Game.Instance.GetTile(Corners[CORNER_TOP_LEFT]).Walkable) {
+                Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_LEFT]));
+                if (intersection.Width * intersection.Height > 0) {
+                    Position.Y = intersection.Bottom;
+                    velocity = Math.Abs(velocity);
+                }
+            }
+            if (!Game.Instance.GetTile(Corners[CORNER_TOP_RIGHT]).Walkable) {
+                Rectangle intersection = Intersections.Rect(Rect, Game.Instance.GetTileRect(Corners[CORNER_TOP_RIGHT]));
+                if (intersection.Width * intersection.Height > 0) {
+                    Position.Y = intersection.Bottom;
+                    velocity = Math.Abs(velocity);
+                }
+            }
+#endif
+        }
+        protected void Animate(float dTime) {
+            animTimer += dTime;
+            if (animTimer > animFPS) {
+                animTimer -= animFPS;
+                currentFrame += 1;
+                if (currentFrame > SpriteSources[currentSprite].Length - 1) {
+                    currentFrame = 0;
+                }
+            }
+        }
+        public void SetJump(float height, float duration) {
+            impulse = 2 * height / duration;
+            impulse *= -1;
+            gravity = -impulse / duration;
+        }
+    }
+}
