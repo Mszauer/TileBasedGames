b76b5eaa1cd2e997f160d02730625bee41401a6b
unknown <mszauer@mail.usf.edu>
2015-09-18 20:13:53 -0500

    initial commit

---
+++ SimpleEnemy/Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using GameFramework;
+using System.Drawing;
+using OpenTK;
+
+namespace SimpleEnemy {
+    class Game {
+        protected Point spawnTile = new Point(2, 1);
+        protected PlayerCharacter hero = null;
+        protected string heroSheet = "Assets/Link.png";
+        public OpenTK.GameWindow Window = null;
+        protected Map room1 = null;
+        protected int[][] room1Layout = new int[][] {
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 1, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 1, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 2 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+        protected Map room2 = null;
+        protected int[][] room2Layout = new int[][] {
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 2, 0, 0, 0, 1, 0, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 0, 0, 0, 0, 0, 1 },
+            new int[] { 1, 0, 0, 0, 1, 0, 0, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+        protected Map currentMap = null;
+        protected string spriteSheets = "Assets/HouseTiles.png";
+        protected Rectangle[] spriteSources = new Rectangle[] {
+            new Rectangle(466,32,30,30),
+            new Rectangle(466,1,30,30),
+            new Rectangle(32,187,30,30)
+        };
+        public Tile GetTile(PointF pixelPoint) {
+            return currentMap[(int)pixelPoint.Y / 30][(int)pixelPoint.X / 30];
+        }
+        public Rectangle GetTileRect(PointF pixelPoint) {
+            int xTile = (int)pixelPoint.X / 30;//integer math
+            int yTile = (int)pixelPoint.Y / 30;
+            Rectangle result = new Rectangle(xTile * 30, yTile * 30, 30, 30);
+            return result;
+        }
+        //Singleton
+        private static Game instance = null;
+        public static Game Instance {
+            get {
+                if (instance == null) {
+                    instance = new Game();
+                }
+                return instance;
+            }
+        }
+
+        protected Game() {
+
+        }
+        public void Initialize(OpenTK.GameWindow window) {
+            Window = window;
+            window.ClientSize = new Size(room1Layout[0].Length * 30, room1Layout.Length * 30);
+            TextureManager.Instance.UseNearestFiltering = true;
+
+            hero = new PlayerCharacter(heroSheet, new Point(spawnTile.X * 30, spawnTile.Y * 30));
+            room1 = new Map(room1Layout, spriteSheets, spriteSources, 2, 0);
+            room2 = new Map(room2Layout, spriteSheets, spriteSources, 0, 2);
+            room1[4][7].MakeDoor(room2, new Point(1, 1));
+            room2[1][0].MakeDoor(room1, new Point(6, 4));
+            currentMap = room1;
+        }
+        public void Update(float dt) {
+            currentMap = currentMap.ResolveDoors(hero);
+            hero.Update(dt);
+        }
+        public void Render() {
+            currentMap.Render();
+            /*
+            foreach (PointF corner in hero.Corners) {
+                if (!GetTile(corner).Walkable) {
+                    GraphicsManager.Instance.DrawRect(GetTileRect(corner), Color.Violet);
+                }
+                else {
+                    GraphicsManager.Instance.DrawRect(GetTileRect(corner), Color.Blue);
+                }
+            }*/
+            hero.Render();
+
+        }
+        public void Shutdown() {
+            room1.Destroy();
+            room2.Destroy();
+            hero.Destroy();
+        }
+    }
+}
---
+++ SimpleEnemy/Map.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GameFramework;
+
+namespace SimpleEnemy {
+    class Map {
+        protected Tile[][] tileMap = null;
+
+        public Tile[] this[int i] {
+            get {
+                return tileMap[i];
+            }
+        }
+        public int Length {
+            get {
+                return tileMap.Length;
+            }
+        }
+        public Map(int[][] layout, string sheets, Rectangle[] sources, params int[] walkable) {
+            Tile[][] result = new Tile[layout.Length][];
+            float scale = 1.0f;
+            for (int i = 0; i < layout.Length; i++) {
+                result[i] = new Tile[layout[i].Length];
+
+                for (int j = 0; j < layout[i].Length; j++) {
+                    Rectangle source = sources[layout[i][j]];
+
+                    Point worldPosition = new Point();
+                    worldPosition.X = (int)(j * source.Width);
+                    worldPosition.Y = (int)(i * source.Height);
+                    result[i][j] = new Tile(sheets, source);
+                    result[i][j].Walkable = false;
+                    result[i][j].IsDoor = false;
+                    result[i][j].WorldPosition = worldPosition;
+                    result[i][j].Scale = scale;
+                    foreach (int w in walkable) {
+                        if (layout[i][j] == w) {
+                            tileMap[i][j].Walkable = true;
+                        }
+                    }
+                }
+                tileMap = result;
+            }
+        }
+        public Map ResolveDoors(PlayerCharacter hero) {
+            Map result = this;
+            for (int row = 0; row < tileMap.Length; row++) {
+                for (int col = 0; col < tileMap[row].Length; col++) {
+                    if (tileMap[row][col].IsDoor) {
+                        //get doors bouding rectangle
+                        Rectangle doorRect = new Rectangle(col * 30, row * 30, 30, 30);
+                        //get a small rectangle in center of the player
+                        Rectangle playerCenter = new Rectangle((int)hero.Center.X - 2, (int)hero.Center.Y - 2, 4, 4);
+
+                        //look for an intersection
+                        Rectangle intersection = Intersections.Rect(doorRect, playerCenter);
+                        if (intersection.Width * intersection.Height > 0) {
+                            result = tileMap[row][col].DoorTarget;
+                            hero.Position.X = tileMap[row][col].DoorLocation.X * 30;
+                            hero.Position.Y = tileMap[row][col].DoorLocation.Y * 30;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        public void Render() {
+            for (int h = 0; h < tileMap.Length; h++) {
+                for (int w = 0; w < tileMap[h].Length; w++) {
+                    tileMap[h][w].Render();
+                }
+            }
+        }
+        public void Destroy() {
+            for (int h = 0; h < tileMap.Length; h++) {
+                for (int w = 0; w < tileMap[h].Length; w++) {
+                    tileMap[h][w].Destroy();
+                }
+            }
+        }
+    }
+}
