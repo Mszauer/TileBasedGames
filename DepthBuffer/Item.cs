5f3d24b37d3ac51f2370d3f70bb383fe98dcdfff
unknown <mszauer@mail.usf.edu>
2015-09-25 15:12:03 -0500

    initial commit, new gfx manager broke graphics

---
+++ DepthBuffer/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GameFramework;
+
+namespace DepthBuffer {
+    class Item {
+        protected int Sprite;
+        protected Rectangle Source;
+        public int Value { get; private set; }
+        public Point Position;
+
+        public Rectangle Rect {
+            get {
+                return new Rectangle(Position.X, Position.Y, Source.Width, Source.Height);
+            }
+        }
+        public Item(string spriteSheet, Rectangle sourceRect, int value, Point position) {
+            Sprite = TextureManager.Instance.LoadTexture(spriteSheet);
+            Source = sourceRect;
+            Value = value;
+            Position = position;
+        }
+        public void Render(PointF offsetPosition) {
+            Point renderPosition = new Point((int)Position.X, (int)Position.Y);
+            renderPosition.X -= (int)offsetPosition.X;
+            renderPosition.Y -= (int)offsetPosition.Y;
+            TextureManager.Instance.Draw(Sprite, renderPosition, 1.0f, Source);
+        }
+        public void Destroy() {
+            TextureManager.Instance.UnloadTexture(Sprite);
+        }
+    }
+}
